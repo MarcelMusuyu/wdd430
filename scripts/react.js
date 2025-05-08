d847a05453cc1b7ab0746152cf2ba315ca7dd891
Marcel Nyembo <marcelmusuyu9@gmail.com>
2025-05-08 09:34:15 +0000

    Adding Interactive with states

--- scripts/react.js
+++ scripts/react.js
@@ -1,9 +1,18 @@
 const domNode = document.getElementById('app');
 const root = ReactDOM.createRoot(domNode);
 
+
 function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
- 
+  const [likes, setLikes] = React.useState(0);
+  function handleClick() {
+    if(window.confirm('Do you want to increment the like count?')) {
+        setLikes(likes + 1);
+        
+    }else {
+        alert('cancelled')
+    } ;
+  }
   return (
     <div>
       <Header title="Develop. Preview. Ship." />
@@ -12,6 +21,7 @@ function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
+       <button onClick={handleClick}>Like {likes}</button>
     </div>
   );
 }
